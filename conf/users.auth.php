7323d985b84c51173192abb505f13e0ed1aa5fe2
FFichtner <fichtner@fim.uni-passau.de>
2017-06-13 19:56:40 +0200

    first commit

---
+++ conf/users.auth.php
@@ -0,0 +1,24 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$ekV2CxzP$52dY0NELEsKA9E.oojmFY0:Dominik Köberl:dominik.koeberl10@yahoo.de:admin,user
+catering:$1$cbY8KEB5$gZJEZ7KGOr0Qipo9koWlT1:Mitarbeiter Beispiel:c164981@mvrht.net:catering
+raumplanung:$1$15r93xUb$0oa.hKOTdDRptP7eb6E89.:Beispiel Beispiel:c164981@mvrht.net:raumplanung
+einladung:$1$6nHTwaXk$IJ61WKHBuTcqs6qZbROKo.:Beispiel Beispiel:c164981@mvrht.net:einladung
+vortrag:$1$jRz2iqIi$5wZuOsaqW.5v1nrQvyoxI1:Beispiel Beispiel:c164981@mvrht.net:vortrag
+flyer:$1$EJFznJmI$As9b64BKqhExXIJCRyPnN.:Beispiel Beispiel:c164981@mvrht.net:flyer
+anmeldung:$1$qRaY60cH$31tT7W3t6yxkEGmXwOczS/:Beispiel Beispiel:c164981@mvrht.net:anmeldung
+admin_catering:$1$hnMFkjqI$sygJNA4Rly54XqX6X01J/1:Beispiel Beispiel:c173034@mvrht.net:catering_admin
+admin_anmeldung:$1$IWtf82Db$B8Z26Nzl4hKP/kpjbl0qQ/:Beispiel Beispiel:c173034@mvrht.net:anmeldung_admin
+admin_einladung:$1$vR4cSW1V$fXsCReSy6ILhJD5.88xTW0:Beispiel Beispiel:c173034@mvrht.net:einladung_admin
+admin_raumplanung:$1$GREnzfiw$PJ9d9.UWLIekw3.kZ/DV3/:Beispiel Beispiel:c173034@mvrht.net:raumplanung_admin
+admin_flyer:$1$OIhcoqKP$NHn0Lsuw4VQO/BS/ULLE1/:Beispiel Beispiel:c173034@mvrht.net:flyer_admin
+admin_vortrag:$1$Iur0PTAF$wTRgvAyXXiVWUsgaS5aTw/:Beispiel Beispiel:c173034@mvrht.net:vortrag_admin
